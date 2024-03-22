7024e5928c5d12b31cc2a653f3b2b086099487f6
bhuneshkansal <bhunesh.kansal@gmail.com>
2024-03-22 19:08:28 +0530

    buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
-import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
+  return (
+    <div>
+      <Button>Clickme</Button>
+    </div>
+  );
 }
